dbb4ae9fd1b4b8d964ce1c5eb9daa5ef457e6992
Skorpx <sdn2000@bk.ru>
2019-12-19 22:34:31 +0300

    XOR_ECR_DECR

--- ecryption.py
+++ ecryption.py
@@ -1,4 +1,26 @@
-def encrypt(k, m):
-    return ''.join(map(chr, [x + k for x in map(ord, m)]))
-
-print(encrypt(2, 'Hello!'))
+def Encrypt_XOR(key,iv,m):
+    string = ''
+    reset_iv = ''
+    for i in range (len(m)):
+        string += chr(ord(m[i]) ^ ord(key[i % len(k)]) ^ ord(iv[i % len(iv)]))
+        reset_iv += chr(ord(m[i]) ^ ord(key[i % len(k)]) ^ ord(iv[i % len(iv)]))
+        if len(reset_iv) == len(key):
+            iv = reset_iv
+            reset_iv = ''
+    return string
+def Decrypt_XOR(key,iv,m):
+    string = ''
+    reset_iv = ''
+    for i in range (len(m)):
+        reset_iv += m[i]
+        string += chr(ord(m[i]) ^ ord(key[i % len(k)]) ^ ord(iv[i % len(iv)]))
+        if len(reset_iv) == len(key):
+            iv = reset_iv
+            reset_iv = ''
+    return string
+m = 'Affdfsfw'
+k = 'KEY'
+i = 'OMG'
+l = Encrypt_XOR(k,i,m)
+print(l)
+print(Decrypt_XOR(k,i,l))
