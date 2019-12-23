b073c0e650a5d384824bf6f7859e7462ebfede60
Nosy <mamukov2000@mail.ru>
2019-12-23 22:18:13 +0300

    я устал немного

--- ecryption.py
+++ ecryption.py
@@ -1,6 +1,8 @@
 #
 #
 from random import randint
+from re import findall
+
 
 def encrypt(k, m):
     return ''.join([chr((ord(x) + k) % 65536) for x in m])
@@ -10,44 +12,38 @@ def decrypt(k, m):
 
 def nonkey(m):
     max_char = 0
-    key = 0
-    
+    key = 0 
     for i in range(65536):
         tt = len(m.split(chr((32 + i) % 65536)))
-        
         if tt > max_char:
             max_char = tt
-            key = i
-            
+            key = i            
     return key
 
-def vernam(m):
-    k = ''
-    tt = ''
-    fin = ''
+def regular(text):
+    template = r"[0-9]+"
+    return findall(template, text)
 
+def vernam(m, tt = '', k = ''):
     for i in m:
-        k = randint(0,32)
-        tt += str(k) + "/"
-        fin += chr((ord(i) + k - 17)%33 + ord('А'))
-    return [fin, tt]
-        
-#def dever(m, k):
-#    k =  k.split('/')
-#    m = ''
-#    for i, s in enumerate(m):
-#        if k[i] != '':
-#            m += chr((ord(s) - int(k[i])- 17)%33 + ord('А'))
-#    return m
+            key = randint(0,32); k += str(key) + "/"
+            tt += chr((ord(i) + key - 17)%33 + ord('А'))
+    return [tt, k]
+    
+def dever(m, k, tt = ''):
+        for index, i in enumerate(m):
+            tt += chr((ord(i) - int(regular(k)[index]) - 17)%33 + ord('А'))
+        return tt
 
-a = vernam('Владимирский централ, ветер северный. Этапом из твери - зла немеренно. Лежит на средце тяжкий груз!')
-print(a[0])
-b = dever(a[0], a[1])
-print(b)
-
-#c = 6
-#a = encrypt(c, 'Владимирский централ, ветер северный. Этапом из твери - зла немеренно. Лежит на средце тяжкий груз!')
+#x = 6
+#a = encrypt(x, 'Натуральный блондин! На всю страну такой один!')
 #print(a)
-#b = nonkey(a)
+#c = nonkey(a)
+#print(decrypt(x, a))
 #print(decrypt(c, a))
-#print(decrypt(b, a))
+
+
+#a = vernam('Листья с клена падают, с ясеня. Ничего себе, ничего себе! Вот смотрю я и действительно. Хорошо, хорошо!')
+#print(a[0])
+#b = dever(a[0], a[1])
+#print(b)
