ca7a5a8122f2974ca2ea49a2e535bb570de2f5ac
Арина Сырченко <syrchenkoarina@gmail.com>
2019-12-27 16:03:31 +0300

    commit1

---
+++ Виженер.py
@@ -0,0 +1,19 @@
+from itertools import cycle
+
+
+ def encryption(word, key, decode: bool = False, clen: int = 65536) -> str:
+
+
+     e, d = lambda x: chr((ord(x[0]) + ord(x[1])) % clen), lambda x: chr((ord(x[0]) - ord(x[1]) + clen) % clen)
+     return "".join(map(d if decode else e, zip(word, cycle(key))))
+
+
+ if __name__ == "__main__":
+
+     word = "my text"
+     key = "key"
+
+     encrypted_text = encryption(word, key)
+     decrypted_text = encryption(encrypted_text, key, decode=True)
+
+     print(f"{word} -> «{encrypted_text}» -> {decrypted_text}")
---
+++ Цезарь.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+
+ def decrypt(word: str or list) -> str:
+     counter = defaultdict(int)
+     key, j = "", 0
+     for i in word:
+         counter[i] += 1
+         if counter[i] > j:
+             key, j = i, counter[i]
+     return chr(ord(key) - ord(" "))
+
+
+ def encryption(word: str, key: str, decode: bool = False, clen: int = 65536) -> str:
+     if len(key) != 1:
+         raise ValueError
+
+     return "".join(chr((ord(i) - ord(key) if decode else (ord(i) + ord(key))) % clen) for i in word)
+
+
+ if __name__ == '__main__':
+     text = "my text"
+     key = "key"
+
+     encrypted_text = encryption(text, key)
+     decrypted_text = encryption(encrypted_text, key, decode=True)
+
+     print(f"{text} -> «{encrypted_text}» -> {decrypted_text}")
+
+     print(f"Подобранный ключ: {decrypt(encrypted_text)}")
